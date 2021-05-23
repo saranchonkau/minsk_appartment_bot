ff4ae50758b6d5fe0159ad6a30be9ac7515b2e99
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2021-05-24 02:04:35 +0300

    Fix date formatting

--- index.ts
+++ index.ts
@@ -2,12 +2,28 @@ import { config } from "dotenv";
 import { createServer } from "http";
 import { ApartmentModel, getApartments } from "./api/apartments";
 import { sendMessage } from "./api/sendMessage";
+import { Temporal } from "proposal-temporal";
 
 config();
 
 const server = createServer();
 
 let newApartment: ApartmentModel | null = null;
+
+function formatDate(isoDate: string): string {
+  const instant = Temporal.Instant.from(isoDate);
+  return instant.toZonedDateTimeISO("Europe/Minsk").toLocaleString("ru-BY");
+}
+
+function convertApartmentToMessage(apartment: ApartmentModel): string {
+  const rows = [
+    `Ссылка: ${apartment.url}`,
+    `Цена: ${apartment.price.converted.USD.amount} USD (${apartment.price.converted.BYN.amount} BYN)`,
+    `Обновлено: ${formatDate(apartment.last_time_up)}`,
+  ];
+
+  return rows.join("\n");
+}
 
 function checkNewApartment() {
   getApartments().then((response) => {
@@ -17,15 +33,7 @@ function checkNewApartment() {
 
     newApartment = latestApartment;
 
-    const rows = [
-      `Ссылка: ${newApartment.url}`,
-      `Цена: ${newApartment.price.converted.USD.amount} USD (${newApartment.price.converted.BYN.amount} BYN)`,
-      `Обновлено: ${new Date(newApartment.last_time_up).toLocaleString(
-        "ru-BY"
-      )}`,
-    ];
-    const message = rows.join("\n");
-
+    const message = convertApartmentToMessage(newApartment);
     console.log("New Apartment: ");
     console.log(message);
 
