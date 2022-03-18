d6ca492341104dd29fda7412e4f73d222ab69938
Ivan Saranchonkau <ivan.saranchenkov@gmail.com>
2022-03-18 11:05:10 +0300

    send notification before exit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { createServer, IncomingMessage, ServerResponse } from "http";
 import { config } from "./config";
 import * as pc from "picocolors";
 import { OnlinerService } from "./modules/onliner/onlinerService";
+import { sendMessage } from "./utils/sendMessage";
 
 function handleRequest(req: IncomingMessage, res: ServerResponse) {
   if (req.url === "/users") {
@@ -33,7 +34,13 @@ function handleSignal() {
 process.on("SIGINT", handleSignal);
 process.on("SIGTERM", handleSignal);
 
-process.on("exit", () => {
+process.on("exit", async () => {
+  const user = config.users[0]!;
+
+  await sendMessage({
+    botToken: user.config.bot_token,
+    chatId: user.config.chat_id,
+    message: "App exit !",
+  });
   console.log(pc.gray("App exit"));
 });
-
